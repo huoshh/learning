480f1dcaa03dd53779de6f58d4e26fd933311fd9
huoshh <huoshuhao@idriverplus.com>
2016-08-24 18:03:23 +0800

    try to push other repository

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/huoshh/learning/src
+# Build directory: /home/huoshh/learning/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(velodyne/velodyne)
+SUBDIRS(velodyne/velodyne_msgs)
+SUBDIRS(velodyne/velodyne_driver)
+SUBDIRS(velodyne/velodyne_pointcloud)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/huoshh/learning/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/huoshh/learning/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/huoshh/learning/devel/lib:/home/huoshh/learning/devel/lib/x86_64-linux-gnu:/opt/serial/lib/x86_64-linux-gnu:/opt/ros/jade/lib/x86_64-linux-gnu:/opt/serial/lib:/opt/ros/jade/lib"
+export PATH="/home/huoshh/learning/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/huoshh/learning/devel/lib/pkgconfig:/home/huoshh/learning/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/serial/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/jade/lib/x86_64-linux-gnu/pkgconfig:/opt/serial/lib/pkgconfig:/opt/ros/jade/lib/pkgconfig"
+export PWD="/home/huoshh/learning/build"
+export PYTHONPATH="/home/huoshh/learning/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/huoshh/learning/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/huoshh/learning/src:$ROS_PACKAGE_PATH"
